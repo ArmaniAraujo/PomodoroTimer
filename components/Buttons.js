7d9edc6fb0682446e6aaf9f31377caec7e306ef5
Armani Araujo <armani@Armanis-MacBook-Air.local>
2020-06-29 16:34:26 -0400

    added button component

---
+++ components/Buttons.js
@@ -0,0 +1,16 @@
+import React, { useState, Component } from 'react';
+import { Button } from 'react-native-elements';
+
+
+const Buttons = () => {
+    return ( 
+        <div>
+            <Button
+                title='+'
+            />
+        </div>    
+        
+    );
+}
+ 
+export default Buttons;
