f3f2d73dac374a0094e9aea548fc4fc73a5863de
Armani <armanidarren@gmail.com>
2020-07-13 23:58:04 -0400

    Button type and style changes

--- components/ButtonUI.js
+++ components/ButtonUI.js
@@ -1,22 +1,50 @@
 import React, { useState, Component } from 'react';
 import { Button } from 'react-native-elements';
-import { TouchableOpacity, Text, View } from 'react-native';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
 
 
 class ButtonUI extends Component {
     render() {
         return (
-            <View>
-                <Button 
-                    title={this.props.title}
-                    onPress={this.props.onPress}
-                    style={[this.props.style, {margin: 8}]}
-                    value={this.props.value}
-                    type='solid'
-                />
+            <View style={styles.circle}>
+            <TouchableOpacity onPress={this.props.onPress}>
+                <Text style={styles.textStyle}>{this.props.title}</Text>
+        </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    circle: {
+        width: 60,
+        height: 50,
+        borderRadius: 100/2,
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2,
+        borderColor: "rgba(94,84,115, .5)",
+    },
+    textStyle: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold',
+
+    }
+
+})
+
+//<Button 
+ //                   title={this.props.title}
+   //                 onPress={this.props.onPress}
+     //               style={[this.props.style, {margin: 10}]}
+       //             value={this.props.value}
+         //           type='clear'
+           //     />
+
+
+
+
 
 export default ButtonUI;
